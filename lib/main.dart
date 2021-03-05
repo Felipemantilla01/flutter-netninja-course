3e8121a60b1a0f7b7eab6774e3fa959e0b0cb248
Felipe Mantilla <felipemantilla@outlook.es>
2021-03-05 15:45:44 -0500

    [feature] create a Scaffold and his basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_net_ninja_course/shared/MyAppBar.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('Hola mundo'),
-));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Hola Mundo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  String title;
+  MyHomePage({this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hola mundo'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    );
+  }
+}
