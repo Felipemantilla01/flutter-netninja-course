5f85e74c8234107b72e376f68c27809a94d1cb9e
Felipe Mantilla <felipemantilla@outlook.es>
2021-03-07 04:51:29 -0500

    [feature] convert the main/home widget into a stateful widget and use a floating action button to change a level

--- lib/main.dart
+++ lib/main.dart
@@ -20,21 +20,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   String title;
   MyHomePage({this.title});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text(
-          this.title,
+          this.widget.title,
           style: TextStyle(fontFamily: 'Montserrat'),
         ),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            this.ninjaLevel += 1;
+          });
+        },
+        backgroundColor: Colors.grey[800],
+        child: Icon(Icons.add),
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -64,7 +82,7 @@ class MyHomePage extends StatelessWidget {
             Text('CURRENT NINJA LEVEL',
                 style: TextStyle(color: Colors.grey, fontFamily: 'Montserrat')),
             SizedBox(height: 10.0),
-            Text('8',
+            Text('$ninjaLevel',
                 style: TextStyle(
                     color: Colors.amberAccent,
                     fontFamily: 'Montserrat',
@@ -80,6 +98,7 @@ class MyHomePage extends StatelessWidget {
                 Text(
                   'felipe.mantilla@kbe.ai',
                   style: TextStyle(
+                      fontFamily: 'Montserrat',
                       fontSize: 18.0,
                       letterSpacing: 1.0,
                       color: Colors.grey[400]),
@@ -89,6 +108,8 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+
     );
   }
 }
+
