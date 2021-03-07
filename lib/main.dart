c3d1b799debbf67b678c713e9176d5df376b706b
Felipe Mantilla <felipemantilla@outlook.es>
2021-03-07 05:31:21 -0500

    [feature] change the project and initialize a list of cards, using extract widgets, and create new dart files for manage classes separately

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_net_ninja_course/shared/MyAppBar.dart';
+import 'quote.dart';
+import 'quoted_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,100 +16,45 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Ninja ID card'),
+      home: QuoteList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  String title;
-  MyHomePage({this.title});
-
+class QuoteList extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        author: 'Oscar Wilde', text: 'Be yourself; everyone is already taken'),
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'I have nothing to declare except my genius'),
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'The truth is rarely pure and never simple'),
+  ];
 
-  int ninjaLevel = 0;
+  Widget quoteTemplate(Quote quote) {
+    return QuoteCard(quote: quote,);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text(
-          this.widget.title,
-          style: TextStyle(fontFamily: 'Montserrat'),
-        ),
+        title: Text('Awesome Quotes' , style: TextStyle(fontFamily: 'Montserrat'),),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            this.ninjaLevel += 1;
-          });
-        },
-        backgroundColor: Colors.grey[800],
-        child: Icon(Icons.add),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/gg.jpeg'),
-                radius: 70,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-            Text('NAME',
-                style: TextStyle(color: Colors.grey, fontFamily: 'Montserrat')),
-            SizedBox(height: 10.0),
-            Text('Felipe Mantilla',
-                style: TextStyle(
-                    color: Colors.amberAccent,
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0)),
-            SizedBox(height: 30.0),
-            Text('CURRENT NINJA LEVEL',
-                style: TextStyle(color: Colors.grey, fontFamily: 'Montserrat')),
-            SizedBox(height: 10.0),
-            Text('$ninjaLevel',
-                style: TextStyle(
-                    color: Colors.amberAccent,
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0)),
-            SizedBox(height: 30.0),
-            Row(
-              children: [
-                Icon(Icons.email, color: Colors.grey[400]),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'felipe.mantilla@kbe.ai',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 18.0,
-                      letterSpacing: 1.0,
-                      color: Colors.grey[400]),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-
+      body: Column(
+          children: quotes
+              .map((e) => quoteTemplate(e))
+              .toList()),
     );
   }
 }
---
+++ lib/quoted_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+
+  Quote quote;
+
+  QuoteCard({this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              this.quote.text,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600], fontFamily: 'Montserrat'),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(this.quote.author,
+                style: TextStyle(fontSize: 14.0, color: Colors.grey[800], fontFamily: 'Montserrat'))
+          ],
+        ),
+      ),
+    );
+  }
+}
